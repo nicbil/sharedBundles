4b43d2bf244fc4eef0ddff0f1e541fd5b8164f5c
Nikolay.Bilenko <nicholas.bilenko@gmail.com>
2017-04-03 14:49:44 +0300

    Start project

---
+++ frontend/views/site/index.php
@@ -0,0 +1,13 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+    <div class="body-content">
+        <div class="row">
+
+        </div>
+    </div>
+</div>
